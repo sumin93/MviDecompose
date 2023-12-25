9000453070325bb54dfa33548c7714ebd4a16be6
sumin93 <suminfx@mail.ru>
2023-12-26 00:02:19 +0300

    ChildStack and Value

--- app/src/main/java/com/example/mvidecomposetest/presentation/DefaultRootComponent.kt
+++ app/src/main/java/com/example/mvidecomposetest/presentation/DefaultRootComponent.kt
@@ -2,9 +2,12 @@ package com.example.mvidecomposetest.presentation
 
 import android.os.Parcelable
 import com.arkivanov.decompose.ComponentContext
+import com.arkivanov.decompose.router.stack.ChildStack
 import com.arkivanov.decompose.router.stack.StackNavigation
+import com.arkivanov.decompose.router.stack.childStack
 import com.arkivanov.decompose.router.stack.pop
 import com.arkivanov.decompose.router.stack.push
+import com.arkivanov.decompose.value.Value
 import com.example.mvidecomposetest.domain.Contact
 import kotlinx.parcelize.Parcelize
 
@@ -12,11 +15,18 @@ class DefaultRootComponent(
     componentContext: ComponentContext
 ) : RootComponent, ComponentContext by componentContext {
 
-    val navigation = StackNavigation<Config>()
+    private val navigation = StackNavigation<Config>()
 
-    fun child(
+    val stack: Value<ChildStack<Config, ComponentContext>> = childStack(
+        source = navigation,
+        initialConfiguration = Config.ContactList,
+        handleBackButton = true,
+        childFactory = ::child
+    )
+
+    private fun child(
+        config: Config,
         componentContext: ComponentContext,
-        config: Config
     ): ComponentContext {
         return when (config) {
             Config.AddContact -> {
